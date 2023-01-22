9309e03ceff837c0cda3b2665b2105c14b8f1bdf
Ulysses Zhan <ulysseszhan@gmail.com>
2023-01-21 20:50:09 -0800

    Added image covers

---
+++ _plugins/gen_covers.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+module Jekyll::UlyssesZhan
+end
+
+module Jekyll
+	class UlyssesZhan::CoversGenerator < Generator
+		safe true
+		priority :high
+		def generate site
+			site.posts.docs.each do |page|
+				unless page.data['image']
+					image_filename = page.basename.sub /\.md$/, '.png'
+					image_path = File.join site.config['covers_path'], image_filename
+					if File.exist? File.join Dir.pwd, image_path
+						page.data['image'] = image_path
+					end
+				end
+			end
+		end
+	end
+end
